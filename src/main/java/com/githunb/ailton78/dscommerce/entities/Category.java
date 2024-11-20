f81b7b8140d6b10e124a4b02af4bb956cce1d60d
Ailton_78 <sd.ailtonsampaio@gmail.com>
2024-11-20 19:59:34 -0300

    Order

---
+++ src/main/java/com/githunb/ailton78/dscommerce/entities/Category.java
@@ -0,0 +1,7 @@
+package com.githunb.ailton78.dscommerce.entities;
+
+public class Category {
+
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/githunb/ailton78/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.githunb.ailton78.dscommerce.entities;
+
+public enum OrderStatus {
+WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+
+}
