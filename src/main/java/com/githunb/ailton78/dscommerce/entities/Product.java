c5ebca955f897f04027cefd0e323c4ee09ab256f
Ailton_78 <sd.ailtonsampaio@gmail.com>
2024-11-21 11:45:46 -0300

    equals, hashcode

--- src/main/java/com/githunb/ailton78/dscommerce/entities/Product.java
+++ src/main/java/com/githunb/ailton78/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.githunb.ailton78.dscommerce.entities;
 import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -13,7 +14,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Column(columnDefinition = "TEX")
+    @Column(columnDefinition = "TEXT")
     private String description;
     private Double price;
     private String imgUrl;
@@ -86,5 +87,19 @@ public class Product {
     }
     public List<Order> getOrders() {
         return items.stream().map(x -> x.getOrder()).toList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
