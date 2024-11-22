71b7c4eaedff23b5e1cb82d0c483af783014cbc5
Ailton_78 <sd.ailtonsampaio@gmail.com>
2024-11-22 13:18:39 -0300

    PUT updade

--- src/main/java/com/githunb/ailton78/dscommerce/controllers/ProductController.java
+++ src/main/java/com/githunb/ailton78/dscommerce/controllers/ProductController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
-
 
 @RestController
 @RequestMapping(value = "/products")
@@ -36,6 +34,12 @@ public class ProductController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 
 
