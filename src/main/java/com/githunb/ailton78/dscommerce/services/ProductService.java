eafd181487d9736f4d5fed4f6ac904b8d3c28f80
Ailton_78 <sd.ailtonsampaio@gmail.com>
2024-11-22 11:24:47 -0300

    get product paged

--- src/main/java/com/githunb/ailton78/dscommerce/services/ProductService.java
+++ src/main/java/com/githunb/ailton78/dscommerce/services/ProductService.java
@@ -4,8 +4,12 @@ import com.githunb.ailton78.dscommerce.dto.ProductDTO;
 import com.githunb.ailton78.dscommerce.entities.Product;
 import com.githunb.ailton78.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -14,10 +18,14 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
-      //Optional<Product> result = repository.findById(id);
         Product product = repository.findById(id).get();
-      //  Product product = result.get();
-       // ProductDTO dto = new ProductDTO(product);
     return new ProductDTO(product);
     }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+       Page<Product> result= repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
+
 }
