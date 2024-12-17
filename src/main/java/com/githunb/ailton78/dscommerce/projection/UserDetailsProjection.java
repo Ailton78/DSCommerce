55e9c029721cef0770fde4f28d8b83ba46c87699
Ailton_78 <sd.ailtonsampaio@gmail.com>
2024-12-16 21:19:39 -0300

    UserDetailsService

---
+++ src/main/java/com/githunb/ailton78/dscommerce/projection/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.githunb.ailton78.dscommerce.projection;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
